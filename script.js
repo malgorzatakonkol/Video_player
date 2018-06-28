9b016c58d95f75e2bdebf885f156fc7f7148f6c2
Małgorzata Konkol <kalafar.malgorzata@gmail.com>
2018-06-28 22:12:22 +0200

    work on layout

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 $(function () {
-   console.log("DOM");
+    console.log("DOM");
 
     var url = 'http://localhost:3000/movie';
-    var ulElemen = $(".movie_list");
-    var video = $(".video");
+    var mainDiv = $(".container");
 
 
     function findMovie(inputVal) {
@@ -13,28 +12,30 @@ $(function () {
             method: "GET"
         }).done(function (response) {
             console.log(response);
-            var tmp = response;
-            var as=$(tmp).filter(function (i,n){
-                if(n){
-                    if(n.title){
-                        if(n.title.toLowerCase().includes(inputValue)){
-                            return n;
+            var as;
+            if (inputValue !== "" && inputValue !== " ") {
+                var tmp = response;
+                as = $(tmp).filter(function (i, n) {
+                    if (n) {
+                        if (n.title) {
+                            if (n.title.toLowerCase().includes(inputValue)) {
+                                return n;
+                            }
+                        }
+                        if (n.name) {
+                            if (n.name.toLowerCase().includes(inputValue)) {
+                                return n
+                            }
+                        }
+                        if (n.description) {
+                            if (n.description.toLowerCase().includes(inputValue)) {
+                                return n
+                            }
                         }
                     }
-                    if(n.name){
-                        if(n.name.toLowerCase().includes(inputValue)){
-                            return n
-                        }
-                    }
-                    if(n.description){
-                        if(n.description.toLowerCase().includes(inputValue)){
-                            return n
-                        }
-                    }
-                }
 
                 });
-            console.log("as",as);
+            }
             showMovie(as);
         }).fail(function (error) {
             console.log(error);
@@ -42,131 +43,131 @@ $(function () {
     }
 
     function showMovie(element) {
-        for (var i=0; i<element.length; i++) {
-            var nameText = element[i].name;
-            var titleText = element[i].title;
-            var desText = element[i].description;
-            var movieUrl = element[i].url;
+        if (element && element.length > 0) {
+            for (var i = 0; i < element.length; i++) {
+                var nameText = element[i].name;
+                var titleText = element[i].title;
+                var desText = element[i].description;
+                var movieUrl = element[i].url;
 
 
+                console.log(nameText);
+                console.log(titleText);
+                console.log(desText);
+                console.log(movieUrl);
 
-            console.log(nameText);
-            console.log(titleText);
-            console.log(desText);
-            console.log(movieUrl);
+                var newList = $("<ul>");
 
-            var newLi = $("<li>");
-            newLi.attr("id", element[i].id);
+                var newLi = $("<li>");
+                newLi.attr("id", element[i].id);
 
-            var newh3n = $("<h3>");
-            newh3n.text(nameText);
+                var newh3n = $("<h3>");
+                newh3n.text(nameText);
 
-            var newh3 = $("<h3>");
-            newh3.text(titleText);
+                var newh3 = $("<h3>");
+                newh3.text(titleText);
 
-            var newSpan = $("<span>");
-            newSpan.text(desText);
-
-
-            var newDiv = $("<div>");
-
-            // var openButton =$("<embed class='button' width=200 height=200>");
-            // openButton.text("play");
-            // openButton.attr("src", movieUrl);
-
-            var openButton = $("<button>");
-            openButton.text("odtwórz film");
-
-            // var filmik = $("<embed class='button' width=200 height=200 hidden='true'>");
-            // filmik.attr("src", movieUrl);
+                var newSpan = $("<span>");
+                newSpan.text(desText);
 
 
+                var openButton = $("<button>");
+                openButton.text("odtwórz film");
 
 
-            newLi.append(newh3n);
-            newLi.append(newh3);
-            newLi.append(newSpan);
-            newLi.append(openButton);
-            // newLi.append(filmik);
+                newLi.append(newh3n);
+                newLi.append(newh3);
+                newLi.append(newSpan);
+                newLi.append(openButton);
 
 
-            function test(button, url){ //stworzenie buttonu odtwórz film
-                button.on("click", function(){
-                    console.log("url", url);
+                function test(button, url) {
+                    button.on("click", function () {
+                        $(".videoWindow").empty();
+                        console.log("url", url);
+                        var newDiv = $("<div class='videoWindow'>");
+                        var videoDiv = $("<video width='680' height='470' class='video'></video>");
+
+                        var movie = $("<source class='movie' type='video/mp4'>");
+                        movie.attr("src", url);
+                        if (url) {
+                            videoDiv.append(movie);
 
 
-
-                    var movie = $("<source class='movie' type='video/mp4'>");
-                    movie.attr("src", url);
-
-                    video.append(movie);
-
-                    var playButton = $("<input class='play' type=button value='play'>");
-                    var pausaButton = $("<input class='pausa' type=button value='pausa'>");
-                    var stopButton= $("<input class='dtop' type=button value='stop'>");
-                    var slowlyButton = $("<input class='slowly' type=button value='slowly'>");
-                    var nextButton = $("<input class='next' type=button value='jedną klatkę dalej'>");
-                    var backButton = $("<input class='back' type=button value='jedną klatkę wstecz'>");
-
-                    newDiv.append(playButton);
-                    newDiv.append(pausaButton);
-                    newDiv.append(slowlyButton);
-                    newDiv.append(stopButton);
-                    newDiv.append(nextButton);
-                    newDiv.append(backButton);
+                            var playButton = $("<input class='play' type=button value='play'>");
+                            var pauseButton = $("<input class='pausa' type=button value='pause'>");
+                            var stopButton = $("<input class='dtop' type=button value='stop'>");
+                            var slowlyButton = $("<input class='slowly' type=button value='slowly'>");
+                            var nextButton = $("<input class='next' type=button value='jedną klatkę dalej'>");
+                            var backButton = $("<input class='back' type=button value='jedną klatkę wstecz'>");
 
 
+                            newDiv.append(playButton);
+                            newDiv.append(pauseButton);
+                            newDiv.append(slowlyButton);
+                            newDiv.append(stopButton);
+                            newDiv.append(nextButton);
+                            newDiv.append(backButton);
 
-                    playButton.on("click", function () {
-                        video[0].play();
-                        video[0].playbackRate = 1;
-                    });
-
-                    pausaButton.on("click", function () {
-                        video[0].pause()
-                    });
-
-                    stopButton.on("click", function () {
-                        video[0].load()
-                    });
+                            newDiv.append(videoDiv);
 
 
-                    slowlyButton.on("click", function () {
-                        video[0].playbackRate = 5;
-                    });
+                            playButton.on("click", function () {
+                                videoDiv[0].play();
+                                videoDiv[0].playbackRate = 1;
+                            });
 
-                    nextButton.on("click", function(){
+                            pauseButton.on("click", function () {
+                                videoDiv[0].pause()
+                            });
 
-                        if(video[0].paused){
-                            console.log("paused");
-                            var frameTime = 1/24;
-                          video[0].currentTime = Math.min(video[0].duration, video[0].currentTime + frameTime);
-                        }
-                    });
+                            stopButton.on("click", function () {
+                                videoDiv[0].load()
+                            });
 
-                    backButton.on("click", function(){
 
-                        if(video[0].paused){
-                            console.log("paused");
-                            var frameTime = 1/24;
-                            video[0].currentTime = Math.max(0, video[0].currentTime - frameTime);
+                            slowlyButton.on("click", function () {
+                                videoDiv[0].playbackRate = 5;
+                            });
+
+                            nextButton.on("click", function () {
+
+                                if (videoDiv[0].paused) {
+                                    console.log("paused");
+                                    var frameTime = 1 / 24;
+                                    videoDiv[0].currentTime = Math.min(videoDiv[0].duration, videoDiv[0].currentTime + frameTime);
+                                }
+                            });
+
+                            backButton.on("click", function () {
+
+                                if (videoDiv[0].paused) {
+                                    console.log("paused");
+                                    var frameTime = 1 / 24;
+                                    videoDiv[0].currentTime = Math.max(0, videoDiv[0].currentTime - frameTime);
+                                }
+                            });
+
+
+                            mainDiv.append(newDiv);
+
+                        } else {
+                            newDiv.text("Brak URL");
+                            mainDiv.append(newDiv);
                         }
                     })
 
+                }
 
-                    newLi.append(newDiv);
-                    newLi.append(movie);
+                test(openButton, movieUrl);
 
-                    video.append(movie);
-                })
+
+                newList.append(newLi);
+                mainDiv.append(newList);
 
             }
-
-            test(openButton, movieUrl);
-
-
-
-            ulElemen.append(newLi);
+        } else {
+            mainDiv.text("Nie znaleziono wideo");
         }
     }
 
@@ -174,9 +175,9 @@ $(function () {
     var infoInput = $(".description");
 
 
-
     function addToList(e) {
         e.preventDefault();
+        mainDiv.empty();
         var text = infoInput.val().toLowerCase();
         findMovie(text);
     }
